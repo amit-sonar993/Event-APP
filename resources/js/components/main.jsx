e277a74b48ffb9552ae88999d59c0544580e69f1
Amit Sonar <amit.sonar993@gmail.com>
2022-11-16 06:33:46 +0545

    redux store configured

--- resources/js/components/main.jsx
+++ resources/js/components/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Event from './event/Event.jsx'
+import  store  from '../store'
+import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Event />
+    <Provider store={store}>
+      <Event />
+    </Provider>
   </React.StrictMode>
 )
